64d58e162bc4aeec07617475e2b03726028ac1f6
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-22 23:23:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,7 @@ class Song
 
   def initialize(name)
     @name = name
+    self.save 
   end
 
   def self.new_by_filename(file)
@@ -19,6 +20,10 @@ class Song
     self.artist = Artist.find_or_create_by_name(name)
     self.artist.add_song(self)
   end
+  
+  def save 
+    @@all << self 
+  end 
   
   def self.all 
     @@all << self 
