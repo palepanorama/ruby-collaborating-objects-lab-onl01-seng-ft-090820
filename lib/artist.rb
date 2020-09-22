5259781de4ed2056435b6e5f90fa8f7ba1457d9b
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-22 23:16:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,51 +1,23 @@
-class Artist
+  
+class Song
 
-  attr_accessor :name, :songs
-
-  @@all = [] 
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
-    @songs = [] 
   end
 
-  def self.all
-    @@all
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
   end
 
-  def add_song(song)
-    @songs << song
-  end
-
-  def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
-  end
-
-
-  def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
-    end
-  end
-
-  def self.create(name)
-    artist = self.new(name)
-    @@all << artist
-    artist
-  end
-
-
-
-  def save
-    @@all << self
-  end
-
-  def print_songs()
-    puts @songs.collect {|x| x.name}
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 
 end
